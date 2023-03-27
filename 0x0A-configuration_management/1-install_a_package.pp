7bd947ae3f2c6488ba27fdf689a7fec3a895975d
Mubarik <burkatech2021@gmail.com>
2023-03-27 04:54:05 -0700

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,2 @@
-# install puppet-lint -v 2.5.0
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
-}
+# installing flask from pip3 using Puppet
+package{'flask': ensure=>'2.1.0', provider=>'pip3'}
